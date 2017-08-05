3be0b195b56385a16b067a50d8f3f30c81177606
Shen <polossk@126.com>
2017-08-05 10:16:31 +0800

    fix matrix element cache opt

--- src/ch01/0105.cpp
+++ src/ch01/0105.cpp
@@ -54,10 +54,10 @@ Matrix add_mod(const Matrix& a, const Matrix& b, const int64 mod)
 Matrix mul(const Matrix& a, const Matrix& b)
 {
     Matrix ans(a.n, b.m);
-    for (int i = 0; i < a.n; i++) for (int j = 0; j < b.m; j++)
+    for (int i = 0; i < a.n; i++) for (int k = 0; k < a.m; k++)(int j = 0; j < b.m; j++)
     {
         int64 tmp = 0;
-        for (int k = 0; k < a.m; k++)
+        for (int j = 0; j < b.m; j++)
             tmp += a.mat[i][k] * b.mat[k][j];
         ans.mat[i][j] = tmp;
     }
