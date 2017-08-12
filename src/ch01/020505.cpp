87b6a970695146956a31db6ac65a98e7ac110ff2
Shen <polossk@126.com>
2017-08-12 09:56:59 +0800

    update discrete_log and ex_discrete_log

--- src/ch01/020505.cpp
+++ src/ch01/020505.cpp
@@ -1,21 +1,19 @@
-// x ^ y = n (mod m), solve y
-int64 discrete_log(int64 x, int64 n, int64 m)
+// a ^ y = b (mod m), gcd(a, m) = 1, solve y
+int64 discrete_log(int64 a, int64 b, int64 m)
 {
-    map<int64, int> rec;
-    int s = (int)(sqrt((double)m));
-    for (; (int64)s * s <= m; ) s++;
-    int64 cur = 1;
-    for (int i = 0; i < s; i++)
+    map<int64, int64> rec;
+    int64 s = ceil(sqrt(m + 0.5));
+    int64 cur = 1; rec[1] = 0;
+    for (int64 i = 1; i < s; rec[cur] = i++)
     {
-        rec[cur] = i;
-        cur = cur * x % m;
+        cur = cur * a % m;
+        if (cur == b) return i;
     }
-    int64 mul = cur; cur = 1;
-    for (int i = 0; i < s; i++)
+    cur = inv(cur * a % m, m);
+    for (int64 i = 0; i < s; i++)
     {
-        int64 more = (int64)n * pow_mod(cur, m - 2, m) % m;
-        if (rec.count(more)) return i * s + rec[more];
-        cur = cur * mul % m;
+        if (rec.find(b) != rec.end()) return i * s + rec[b];
+        b = b * cur % m;
     }
     return -1;
 }
